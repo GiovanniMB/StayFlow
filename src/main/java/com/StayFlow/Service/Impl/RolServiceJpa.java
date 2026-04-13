7bc159e2f33ad52050f70ce3b9ae5d40d4b30309
Giovanni <317233946@pcpuma.acatlan.unam.mx>
2026-04-12 18:39:43 -0600

    se estructura el proyecto

--- src/main/java/com/StayFlow/Service/Impl/RolServiceJpa.java
+++ src/main/java/com/StayFlow/Service/Impl/RolServiceJpa.java
@@ -1,4 +1,4 @@
-package com.StayFlow.Service;
+package com.StayFlow.Service.Impl;
 
 import java.util.List;
 
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.StayFlow.Repository.RolRepository;
+import com.StayFlow.Service.Interfaces.IRolService;
 import com.StayFlow.model.Rol;
 
 @Service
